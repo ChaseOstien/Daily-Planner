52303b4a95d484d78973eb58f60fd37d712c5a20
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-07-16 15:05:20 -0500

    Added Javascript for for format and local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,7 @@ var hoursInfo = [
 
 var listRoot = $('#listRoot');
 
+
 hoursInfo.forEach(function(info) {
   var div = $('<div>').attr('id', info.hourId).addClass('row time-block');
   var hourDiv = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(info.hourText);
@@ -26,8 +27,102 @@ hoursInfo.forEach(function(info) {
   button.append(icon);
   div.append(hourDiv, textArea, button);
   listRoot.append(div);
-});
 
+    $(document).ready(function(){
+      let jsonData;
+      let data;
+      let textAreaEl = $('#hour-9').children('.description');
+      textAreaEl.val(localStorage.getItem('9AM'));
+      $('#hour-9').children('.saveBtn').click(function() {
+        var eventInfo = $.trim($('#hour-9').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('9AM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-10').children('.description');
+      textAreaEl.val(localStorage.getItem('10AM'));
+      $('#hour-10').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-10').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('10AM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-11').children('.description');
+      textAreaEl.val(localStorage.getItem('11AM'));
+      $('#hour-11').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-11').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('11AM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-12').children('.description');
+      textAreaEl.val(localStorage.getItem('12PM'));
+      $('#hour-12').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-12').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('12PM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-13').children('.description');
+      textAreaEl.val(localStorage.getItem('1PM'));
+      $('#hour-13').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-13').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('1PM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-14').children('.description');
+      textAreaEl.val(localStorage.getItem('2PM'));
+      $('#hour-14').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-14').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('2PM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-15').children('.description');
+      textAreaEl.val(localStorage.getItem('3PM'));
+      $('#hour-15').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-15').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('3PM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-16').children('.description');
+      textAreaEl.val(localStorage.getItem('4PM'));
+      $('#hour-16').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-16').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('4PM', jsonData);
+          }
+        });
+        textAreaEl = $('#hour-17').children('.description');
+      textAreaEl.val(localStorage.getItem('5PM'));
+      $('#hour-17').children('.saveBtn').click(function() {
+        eventInfo = $.trim($('#hour-17').children('.description').val());
+        if (eventInfo !== '') {
+          data = { eventInfo };
+          jsonData = JSON.stringify(eventInfo);
+          localStorage.setItem('5PM', jsonData);
+          }
+        });
+      });
+  });
 
 
 $(function () {
@@ -38,6 +133,9 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
+
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -49,4 +147,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  var today = dayjs();
+  $('#currentDay').text(today.format('dddd, MMMM D'));
 });
